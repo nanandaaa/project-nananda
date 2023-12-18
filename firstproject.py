0fc989c8907ccf733d30ca76899a1d23cf12ba90
nanandaaa <145988174+nanandaaa@users.noreply.github.com>
2023-12-18 23:34:15 +0700

    Update firstproject.py

--- firstproject.py
+++ firstproject.py
@@ -15,8 +15,8 @@ st.write('Frekuensi plot sinus adalah ',f1)
 st.write('Frekuensi plot cosinus adalah ',f2)
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(x)  # Calculating sin(x) values
-z = np.cos(x)  # Calculating sin(x) values
+y = np.sin(f1*x)  # Calculating sin(x) values
+z = np.cos(f2*x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
